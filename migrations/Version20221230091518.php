7616ca7854fcf556b5b70e03d42cf73b22d199e8
Franck <franckvienot7@gmail.com>
2022-12-30 13:55:49 +0100

    rebuilt of the previous version

---
+++ migrations/Version20221230091518.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221230091518 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE convives convives INT NOT NULL, CHANGE nom nom VARCHAR(180) NOT NULL, CHANGE prenom prenom VARCHAR(180) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user CHANGE prenom prenom VARCHAR(60) DEFAULT NULL, CHANGE nom nom VARCHAR(60) DEFAULT NULL, CHANGE convives convives INT DEFAULT NULL');
+    }
+}
