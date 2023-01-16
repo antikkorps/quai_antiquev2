3aab6e46948f7c782019f6e6b43e2cc1de6ef3a1
Franck <franckvienot7@gmail.com>
2023-01-16 22:08:31 +0100

    add a calendar and added a more appealing form

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Reservation;
 use App\Entity\User;
+use App\Entity\Horaire;
+use PhpParser\Node\Stmt\Label;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,9 +20,32 @@ class ReservationType extends AbstractType
         $builder
 
 
-            ->add('date')
-            ->add('nombreDePersonnes')
-            ->add('allergies');
+            ->add('date', DateTimeType::class, [
+                'label' => 'Réserver une table',
+                'help' => 'Date prévue pour votre venue parmi nous',
+                'widget' => 'single_text',
+            ])
+            ->add('nombreDePersonnes', NumberType::class, [
+                'label' => 'Nombre de convives',
+                'help' => 'Merci de nous informer du nombre de convives vous compris',
+            ])
+            ->add('allergies', ChoiceType::class, [
+                'choices' => [
+                    'Rien à signaler' => 1,
+                    'Lait' => 2,
+                    'Oeuf' => 3,
+                    'Fruits à coque' => 4,
+                    'arachide' => 5,
+                    'Moutarde' => 6,
+                    'crustacés' => 7,
+                    'poisson' => 8,
+                    'soja' => 9,
+                    'gluten' => 10,
+                    'Sulfites' => 11,
+                    'Blé' => 12
+                ],
+                'help' => 'Merci de nous informer de vos allergies',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
