61bc872d50fe2ad02317513b54de410ee750aeb8
Franck <franckvienot7@gmail.com>
2023-01-19 23:34:35 +0100

    added a component

--- src/Components/PlacesRestantes.php
+++ src/Components/PlacesRestantes.php
@@ -7,13 +7,25 @@ use App\Repository\ReservationRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('places_restantes')]
-class GrilleHoraire
+class PlaceRestantes
 {
     public HoraireRepository $horaireRepository;
+    public ReservationRepository $reservationRepository;
 
-    public function __construct(HoraireRepository $horaireRepository)
+    public function __construct(HoraireRepository $horaireRepository, ReservationRepository $reservationRepository)
     {
         $this->horaireRepository = $horaireRepository;
+        $this->reservationRepository = $reservationRepository;
+    }
+
+    public function getPlacesRestantes($horaire): int
+    {
+        $placesRestantes = 100;
+        $reservations = $this->reservationRepository->findBy(['horaireDeVenue' => $horaire]);
+        foreach ($reservations as $reservation) {
+            $placesRestantes -= $reservation->getNombreDePersonnes();
+        }
+        return $placesRestantes;
     }
     public function getHoraires(): array
     {
